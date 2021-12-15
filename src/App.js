a6316ceae24aefebf2f998e275596a14604dd766
Raj <varasaniraj5@gmail.com>
2021-12-15 11:02:53 +0530

    Reinitialized project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./Containers/Home";
+
+const App = () => {
+  
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    )
+  
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { ConfigureStore } from "./Redux/ConfigureStore";
+
+const store = ConfigureStore();
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+      <Provider store={store}>
+    
+      
+      <Router>
+      
+        <App />
+      
+      </Router>
+      
+    
+      </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
