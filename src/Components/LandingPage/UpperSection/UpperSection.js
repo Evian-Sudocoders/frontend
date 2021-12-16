a9b133fef7ff0dce776a716a5c3a9b68d32a5f19
Raj <varasaniraj5@gmail.com>
2021-12-16 13:48:47 +0530

    Updated landing page upper sec button styles

--- src/Components/LandingPage/UpperSection/UpperSection.js
+++ src/Components/LandingPage/UpperSection/UpperSection.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import Button from "../../Button/Button";
 import styles from "./UpperSection.module.css";
 import { ReactComponent as Logo } from "../../../Assets/_General/evian.svg";
 import { ReactComponent as EV1 } from "../../../Assets/LandingPage/ev1.svg";
 
 function UpperSection() {
+  const history = useHistory();
+
+  const goToSignUp = () => {
+    history.push("/signup");
+  };
+
+  const goToSignIn = () => {
+    history.push("/signin");
+  };
   return (
     <div className={styles.Wrapper}>
       <div className={styles.LogoWrapper}>
@@ -13,10 +24,22 @@ function UpperSection() {
 
       <div className={styles.SubWrapper}>
         <div className={styles.LeftContainer}>
-          <div className={styles.Heading}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, dolorem?</div>
+          <div className={styles.Heading}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero,
+            dolorem?
+          </div>
           <div className={styles.Buttons}>
-            <Button wrapperClass={styles.ButtonStyle1} content="Sign Up" onClick="\signUp" />
-            <Button wrapperClass={styles.ButtonStyle2} isNotBorder content="Sign In" onClick="\signIn"  mainColor={"linear-gradient(63.31deg, #00D1FF -9.99%, #06C4FF -9.98%, rgba(3, 195, 255, 0.23) 131.09%)"}/>
+            <Button
+              wrapperClass={styles.ButtonStyle1}
+              content="Sign Up"
+              onClick={goToSignUp}
+            />
+            <Button
+              wrapperClass={styles.ButtonStyle2}
+              isNotBorder
+              content="Sign In"
+              onClick={goToSignIn}
+            />
           </div>
         </div>
 
