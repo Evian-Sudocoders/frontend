4cee8c38832c19c1f3dc54678a7b8f6eed9d189f
Raj <varasaniraj5@gmail.com>
2021-12-16 13:50:38 +0530

    Created signin / up page

---
+++ src/Components/StaticData.js
@@ -0,0 +1,35 @@
+export const signUpData = {
+  title: "Sign Up",
+  bottomContent: {
+    upperText: {
+      title: "Already have an account?",
+      link: "Sign In",
+      linkTo: "/signin",
+    },
+    bottomText: {
+      title: "By signing up you agree to IndieFoods's",
+      link: "Terms of Service",
+      link2: " Privacy Policy",
+      linkTo: "/terms",
+      linkTo2: "/privacy",
+    },
+  },
+};
+
+export const signInData = {
+  title: "Sign In",
+  bottomContent: {
+    upperText: {
+      title: "Don't have an account?",
+      link: "Sign Up",
+      linkTo: "/signup",
+    },
+    bottomText: {
+      title: "By clicking on Continue, you agree to IndieFoods's",
+      link: "Terms of Service",
+      link2: " Privacy Policy",
+      linkTo: "/terms",
+      linkTo2: "/privacy",
+    },
+  },
+};
