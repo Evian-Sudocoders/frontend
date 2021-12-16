0ab392eb7201c0d54c3b1c5a849e288954fb3f6f
Harsh Gupta <harshgupta1249@gmail.com>
2021-12-16 18:34:22 +0530

    Dashboard Component Structured

--- src/Components/Dashboard/RightSection/PaymentHistoryIndividual/PaymentHistoryIndividual.js
+++ src/Components/Dashboard/RightSection/PaymentHistoryIndividual/PaymentHistoryIndividual.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import numberWithCommas from '../helper/convertNumberWithComma';
+import React from 'react';
 
 import styles from './PaymentHistoryIndividual.module.css';
 
