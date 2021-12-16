19a04b7bda48be54d663dd561d6015ee6c228d8f
Raj <varasaniraj5@gmail.com>
2021-12-16 11:00:52 +0530

    Created Landing page container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.css";
 
 import { Switch, Route } from "react-router-dom";
-import LowerSection from "./Components/LandingPage/LowerSection/LowerSection";
-import UpperSection from "./Components/LandingPage/UpperSection/UpperSection";
-
-import Home from "./Containers/Home";
-import LandingPage from "./Components/LandingPage/LandingPage";
+import LandingPage from "./Containers/LandingPage";
 
 const App = () => {
   return (
---
+++ src/Containers/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPageMainSec from "../../Components/LandingPage";
+
+import styles from "./LandingPage.module.css";
+
+function LandingPage() {
+  return (
+    <div>
+      <LandingPageMainSec />
+    </div>
+  );
+}
+
+export default LandingPage;
