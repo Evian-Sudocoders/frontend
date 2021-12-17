c95974766d6c3b6d4aa189df90c0637dca641d8c
Harsh Gupta <harshgupta1249@gmail.com>
2021-12-17 16:21:15 +0530

    created dashboard container route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LandingPage from "./Containers/LandingPage";
 import Profile from "./Containers/Profile/Profile";
 import StationInfo from "./Containers/StationInfo";
 import HomePage from "./Containers/HomePage";
+import Dashboard from "./Containers/Dashboard/Dashboard";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/profile" component={Profile} />
         <Route path="/userhome" component={HomePage} />
         <Route path="/station" component={StationInfo} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </>
   );
