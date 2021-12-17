6ed656ba0472e4164fc5734a3f9a57f5b6ebf6ab
Akash Gupta <akash.gupta.anup@gmail.com>
2021-12-17 17:58:05 +0530

    Created InputRequired Static File

---
+++ src/Components/StationInfo/PaymentPopup/InputRequired.js
@@ -0,0 +1,25 @@
+export const inputRequired = [
+  {
+    label: "Battery size  ( in kWh ) :",
+    name: "batterySize",
+  },
+  {
+    label: "Charging power ( in kW ) :",
+    name: "chargingPower",
+  },
+  {
+    label: "Starting charge level  ( in % ) :",
+    name: "startingChargeLevel",
+  },
+  {
+    label: "Target charge level  ( in % ) :",
+    name: "targetChargeLevel",
+  },
+];
+
+export const vechicleInputRequired = [
+  {
+    label: "Vehicle Number",
+    name: "vehicleNumber",
+  },
+];
