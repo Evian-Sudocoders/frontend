bf7ef1f912ae580c1f27c96e28befdb7b9982abb
Raj <varasaniraj5@gmail.com>
2021-12-17 22:33:56 +0530

    Created Station point list item

---
+++ src/Components/StationChargingPointsList/StationChargingPointsListItem/StationChargingPointsListItem.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+
+import Styles from "./StationChargingPointsListItem.module.css";
+
+function StationChargingPointsListItem({
+  data,
+  isEditing,
+  onChangeFun,
+  keyIndex,
+}) {
+  const powerRef = useRef(456);
+  const chargeRef = useRef(789);
+
+  useEffect(() => {
+    if (keyIndex === 1 && isEditing) {
+      powerRef.current.focus();
+    }
+  }, [isEditing]);
+
+  useEffect(() => {
+    powerRef.current.value = data.capacity;
+    chargeRef.current.value = data.cost;
+    updateWidth(powerRef.current);
+    updateWidth(chargeRef.current);
+  }, [data]);
+
+  const handleChange = (e) => {
+    updateWidth(e.target);
+
+    if (isEditing && onChangeFun) {
+      onChangeFun(keyIndex, {
+        id: keyIndex,
+        capacity: powerRef.current.value,
+        cost: chargeRef.current.value,
+      });
+    }
+  };
+
+  const updateWidth = (elem) => {
+    const width = elem.value.length * 0.8 + 2;
+    elem.style.width = width + "em";
+  };
+
+  return (
+    <div className={Styles.Wrapper}>
+      <div className={Styles.LeftWrapper}>
+        <h3 className={Styles.Id}>{data.id}</h3>
+      </div>
+      <div className={Styles.RightWrapper}>
+        <div className={Styles.ListItemWrapper}>
+          <h4 className={Styles.Label}>Power Capacity ( in kW ) :</h4>
+          <input
+            ref={powerRef}
+            type="number"
+            name="Power"
+            value={data.capacity}
+            onChange={handleChange}
+            className={Styles.Input}
+            disabled={!isEditing}
+          />
+        </div>
+        <div className={Styles.ListItemWrapper + " " + Styles.ListItemWrapper2}>
+          <h4 className={Styles.Label}>
+            Charges ( per half an hour / slot ) :
+          </h4>
+          <input
+            ref={chargeRef}
+            type="number"
+            name="Charges"
+            defaultValue={data.cost}
+            onChange={handleChange}
+            className={Styles.Input}
+            disabled={!isEditing}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StationChargingPointsListItem;
