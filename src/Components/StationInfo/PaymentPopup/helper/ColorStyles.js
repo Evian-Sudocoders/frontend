ec15f5adf9fae5422af592828e10ad72f17591a8
Akash Gupta <akash.gupta.anup@gmail.com>
2021-12-17 21:46:18 +0530

    Created color styles for react select

---
+++ src/Components/StationInfo/PaymentPopup/helper/ColorStyles.js
@@ -0,0 +1,65 @@
+export const selectColorStyles = {
+  control: (styles) => ({
+    ...styles,
+    fontSize: "var(--font-17)",
+    maxWidth: "35rem",
+    minWidth: "20rem",
+    fontWeight: "600",
+    border: "2px solid var(--sec-grey)",
+    outline: "none",
+    boxShadow: "none",
+    ":visited": {
+      border: "2px solid var(--sec-grey)",
+    },
+    ":active": {
+      border: "2px solid var(--sec-grey)",
+    },
+    ":hover": {
+      border: "2px solid var(--sec-grey)",
+    },
+  }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    return {
+      ...styles,
+      fontSize: "var(--font-17)",
+      transition: "background-color 0.1s ease",
+      ":active": {
+        ...styles[":active"],
+        backgroundColor: "var(--sec-aqua)",
+      },
+      ":hover": {
+        ...styles[":hover"],
+        backgroundColor: "var(--sec-aqua)",
+      },
+      ":visited": {
+        ...styles[":visited"],
+        backgroundColor: "var(--sec-aqua)",
+      },
+    };
+  },
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    fontSize: "var(--font-17)",
+    backgroundColor: "var(--sec-aqua)",
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    transition: "all 0.2s",
+    backgroundColor: "var(--sec-aqua)",
+
+    ":hover": {
+      backgroundColor: "var(--aqua)",
+      cursor: "pointer",
+      fontSize: "var(--font-16)",
+    },
+  }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    color: "var(--sec-grey)",
+    transition: "all 0.1s",
+
+    ":hover": {
+      color: "var(--ter-black)",
+    },
+  }),
+};
