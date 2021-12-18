aae1b896bf0426bc79b785ef096d8f1c49ad1242
Raj <varasaniraj5@gmail.com>
2021-12-18 15:04:39 +0530

    Updated constants

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,3 +1,8 @@
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = "https://evian.azurewebsites.net/api/v1";
+const AUTH_URL = `${BASE_URL}/auth`;
+const STATION_URL = `${BASE_URL}/station`;
 
-export const EXAMPLE_URL = `${BASE_URL}/examples`;
+export const SIGNUP_USER_URL = `${AUTH_URL}/signupuser`;
+export const SIGNUP_STATION_URL = `${AUTH_URL}/signupstation`;
+export const GET_USER_URL = `${AUTH_URL}/getUserDetails`;
+export const GET_STATION_URL = `${STATION_URL}`;
