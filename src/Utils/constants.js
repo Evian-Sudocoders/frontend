72c8942f96e560184d6c24a318441f2bc76c871e
Akash Gupta <akash.gupta.anup@gmail.com>
2021-12-18 18:30:25 +0530

    constants updated

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,7 +1,8 @@
-const BASE_URL = "https://evian.azurewebsites.net/api/v1";
-const AUTH_URL = `${BASE_URL}/auth`;
-const STATION_URL = `${BASE_URL}/station`;
-const BOOKING_URL = `${BASE_URL}/booking`;
+export const BASE_URL = "https://evian.azurewebsites.net/api/v1";
+export const AUTH_URL = `${BASE_URL}/auth`;
+export const STATION_URL = `${BASE_URL}/station`;
+export const BOOKING_URL = `${BASE_URL}/booking`;
+export const CHARGING_POINT_URL = `${BASE_URL}/chargingPoint/bookedSlots`;
 
 export const SIGNUP_USER_URL = `${AUTH_URL}/signupuser`;
 export const SIGNUP_STATION_URL = `${AUTH_URL}/signupstation`;
@@ -9,4 +10,3 @@ export const GET_USER_URL = `${AUTH_URL}/getUserDetails`;
 
 export const INITIALIZE_BOOKING_URL = `${BOOKING_URL}/initializeBooking`;
 export const VERIFY_BOOKING_URL = `${BOOKING_URL}/verifyBooking`;
-export const GET_STATION_URL = `${AUTH_URL}/getStationDetails`;
