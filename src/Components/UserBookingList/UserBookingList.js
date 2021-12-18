fb084acea72128ff218beb78b7b2441e068305ad
Raj <varasaniraj5@gmail.com>
2021-12-18 12:14:34 +0530

    Removed temp data from user booking list

--- src/Components/UserBookingList/UserBookingList.js
+++ src/Components/UserBookingList/UserBookingList.js
@@ -5,25 +5,7 @@ import UserBookingCard from "./UserBookingCard";
 
 const title = "Your Bookings";
 
-function UserBookingList({
-  bookings = Array(12)
-    .fill({})
-    .map((item, index) => ({
-      id: index,
-      username: "Abc Xyz",
-      vehicle_number: "MH 12 ABC 1234",
-      station: `Lorem Ipsum Electric Vehicle Charging Station`,
-      date: new Date(),
-      address: `${index} Opp Virwani Ind Est Jay Bharat Indl Est 2nd Flr, 44, Goregaon (east), Surat`,
-      pointNo: Math.floor(Math.random() * 6) + 1,
-      slots: Array(Math.floor(Math.random() * 6) + 1)
-        .fill(Math.floor(Math.random() * 15))
-        .map((x, index) => x + index),
-      charges: Math.floor(Math.random() * 100),
-      status: index < 2 ? "pending" : "success",
-    })),
-  openBookingSlide,
-}) {
+function UserBookingList({ bookings, openBookingSlide }) {
   return (
     <div className={Styles.Wrapper}>
       <div className={Styles.Title}>{title}</div>
