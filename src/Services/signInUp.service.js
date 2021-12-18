77ac0e6223deee8e394d7c4509ee8eeff2085d89
Raj <varasaniraj5@gmail.com>
2021-12-18 15:03:48 +0530

    Updated signup service

--- src/Services/signInUp.service.js
+++ src/Services/signInUp.service.js
@@ -1,12 +1,56 @@
+import axios from "axios";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase";
+import { SIGNUP_USER_URL, SIGNUP_STATION_URL } from "../Utils/constants";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+export const signUpUser = async ({ email, password, userData, isStation }) => {
+  console.log(userData, isStation);
+  try {
+    const auth = getAuth();
+    const userCredentialResult = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const accessToken = userCredentialResult.user.accessToken;
+    console.log(userCredentialResult.user);
+    if (isStation) {
+      const { data } = await axios.post(SIGNUP_STATION_URL, userData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(data);
+      return {
+        status: true,
+        message: "Signup Successful",
+        accessToken: accessToken,
+        id: userCredentialResult.user.uid,
+      };
+    } else {
+      const { data } = await axios.post(SIGNUP_USER_URL, userData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log(data);
+      return {
+        status: true,
+        message: "Signup Successful",
+        accessToken: accessToken,
+      };
+    }
+  } catch (error) {
+    console.log(error.code);
+    const message =
+      error.code === "auth/email-already-in-use"
+        ? "User with this email already exists, please login!"
+        : "Something went wrong!";
+
+    return {
+      status: false,
+      message: message,
+    };
+  }
+};
