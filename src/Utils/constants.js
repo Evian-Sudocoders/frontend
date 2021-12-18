3f1b390263557a03a0c0d6ff3d6da643df75e12f
Akash Gupta <akash.gupta.anup@gmail.com>
2021-12-18 21:08:57 +0530

    Added constants for user bookings

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -16,3 +16,5 @@ export const INITIALIZE_BOOKING_URL = `${BOOKING_URL}/initializeBooking`;
 export const VERIFY_BOOKING_URL = `${BOOKING_URL}/verifyBooking`;
 export const UPDATE_PROFILE_PICTURE_URL = `${USER_URL}/updateProfilePicture`;
 export const UPDATE_ADDRESS_URL = `${STATION_URL}/updateAddress`;
+
+export const GET_USER_BOOKINGS = `${USER_URL}/bookings`;
