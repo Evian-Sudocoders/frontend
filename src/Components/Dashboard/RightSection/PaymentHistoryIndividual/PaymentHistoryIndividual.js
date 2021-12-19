df86bdf080f8c9417f88fd695063f6bbc37bbdb3
Raj <varasaniraj5@gmail.com>
2021-12-19 14:59:16 +0530

    Updated PaymentHistoryIndividual

--- src/Components/Dashboard/RightSection/PaymentHistoryIndividual/PaymentHistoryIndividual.js
+++ src/Components/Dashboard/RightSection/PaymentHistoryIndividual/PaymentHistoryIndividual.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import styles from './PaymentHistoryIndividual.module.css';
+import styles from "./PaymentHistoryIndividual.module.css";
 
 function PaymentHistoryIndividual({ paymentDetails }) {
-    return (
-        <div className={styles.PaymentHistoryIndividualWrapper}>
-            <p className={styles.PaymentUserName}>{paymentDetails.userName}</p>
-            <p className={styles.PaymentAmount}> {`₹${paymentDetails.amount}`}</p>
-            <p className={styles.PaymentTimestamp}>{paymentDetails.timestamp.toLocaleString()}</p>
-        </div>
-    );
+  return (
+    <div className={styles.PaymentHistoryIndividualWrapper}>
+      <p className={styles.PaymentUserName}>{paymentDetails.userName}</p>
+      <p className={styles.PaymentAmount}> {`₹ ${paymentDetails.totalAmount}`}</p>
+      <p className={styles.PaymentTimestamp}>
+        {paymentDetails.startDate.toLocaleString()}
+      </p>
+    </div>
+  );
 }
 
-export default PaymentHistoryIndividual
+export default PaymentHistoryIndividual;
